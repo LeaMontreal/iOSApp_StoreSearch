39ffbc042d3e2156f8e3e55150486616f8aac222
lilian <wanglijun00123172@gmail.com>
2021-11-03 11:16:00 -0400

    Initial Commit

---
+++ StoreSearch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StoreSearch
+//
+//  Created by user206341 on 11/3/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
